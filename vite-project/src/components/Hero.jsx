f0224f3bea230e88e9c49cfda641165823f8c417
Aparna <aparnaraman995@gmail.com>
2025-10-31 17:48:48 +0530

    hero-section changed

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -10,10 +10,17 @@ const Hero = () => {
       </div>
 
       <h1 className="text-6xl">
-        Turning imagination into <span className="text-blue-600">digital</span>{" "}
+        Turning imagination into <span className="text-blue-600">digital</span>{" "}impact
       </h1>
+        <p className="text-center text-lg text-gray-600 max-w-2xl">
+        We help you create stunning digital experiences that drive results.
+      </p>
+      <div className="relative">
+        <img src={assets.hero_img} className="w-full max-w-6xl"/>
+        <img src={assets.bgImage1} className="absolute -top-60  -right-40 -z-1"/>
+        </div>
     </div>
-  );
+  );    
 };
 
 export default Hero;
